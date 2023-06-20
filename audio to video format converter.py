5d576ebe6d26390a081559455ad06bdd9032eb84
QAAAK <dmitsantalov@yandex.ru>
2023-06-20 15:21:39 +0300

    Hello! Thanks you for watching my project!

---
+++ audio to video format converter.py
@@ -0,0 +1,13 @@
+import moviepy.editor
+
+
+from pathlib import Path
+
+video_file = Path('video.mp4')
+
+video = moviepy.editor.VideoClip(f'{video_file}')
+# video = moviepy.editor.VideoClip('video.mp4')
+audio = video.audio
+
+# audio.write_audiofile('audio.mp3')
+audio.write_audiofile(f'{video_file.stem}.mp3')
